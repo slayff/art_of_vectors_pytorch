4dfe2a861b82317152c8ecc18d4e6a94fc10f1fc
MovElb <mosik454@mail.ru>
2019-12-19 14:06:01 +0300

    fix

--- art_of_vectors/adversarial_attack.py
+++ art_of_vectors/adversarial_attack.py
@@ -7,7 +7,6 @@ from .power_method import PowerMethod, JacobianOperator
 class AdversarialAttack:
     def __init__(
             self,
-            model_feature_extracter,
             input_shape,
             output_shape,
             p=float('inf'),
@@ -16,7 +15,6 @@ class AdversarialAttack:
             device=torch.device('cpu'),
             verbose=0
     ):
-        self.mfe = model_feature_extracter
         self.input_shape = input_shape
         self.input_dim = torch.prod(torch.tensor(input_shape)).item()
         self.hidden_dim = torch.prod(torch.tensor(output_shape)).item()
@@ -24,9 +22,9 @@ class AdversarialAttack:
         self.device = device
         self.verbose = verbose
 
-    def fit(self, img_iter, n_batches=1):
-        self.mfe.to(self.device)
-        self.mfe.eval()
+    def fit(self, mfe, img_iter, n_batches=1):
+        mfe.to(self.device)
+        mfe.eval()
 
         for i, img_data in enumerate(img_iter):
             if i >= n_batches:
@@ -36,23 +34,17 @@ class AdversarialAttack:
                 print(f'Running power method on batch #{i}')
 
             img_batch = img_data['image'].to(self.device)
-            jac = JacobianOperator(img_batch, self.mfe, self.input_dim, self.hidden_dim, self.device)
+            jac = JacobianOperator(img_batch, mfe, self.input_dim, self.hidden_dim, self.device)
             self.power_method.fit(jac)
 
-    @staticmethod
-    def fooling_rate(model_raw_pred, model_pert_pred):
-        return (model_raw_pred != model_pert_pred).float().mean().item()
-    
-    @staticmethod
-    def predict_raw(mfe, img_iter, device=torch.device('cpu'), verbose=0):
-        mfe.to(device)
+    def predict_raw(self, mfe, img_iter):
+        mfe.to(self.device)
         mfe.eval()
 
         probs = []
         preds = []
-        for i, img_data in enumerate(tqdm(img_iter, disable=not verbose)):
-            
-            img_batch = img_data['image'].to(device)
+        for i, img_data in enumerate(tqdm(img_iter, disable=not self.verbose)):
+            img_batch = img_data['image'].to(self.device)
             probabilities = torch.softmax(mfe(img_batch), dim=-1)
             cur_probs, cur_preds = torch.max(probabilities, dim=-1)
             
@@ -63,3 +55,8 @@ class AdversarialAttack:
 
     def get_perturbation(self, adv_norm=10):
         return self.power_method.get_perturbation(self.input_shape, adv_norm=adv_norm)
+
+    @staticmethod
+    def fooling_rate(model_raw_pred, model_pert_pred):
+        return (model_raw_pred != model_pert_pred).float().mean().item()
+
